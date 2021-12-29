c74afcca12af76cb53aba6d1966c3137a73c8627
cwang5336 <cwang5336@gmail.com>
2021-12-29 18:37:30 -0500

    second

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,14 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  var c = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === true || arr[i] === false || typeof arr[i] === "string") {
+      c++;
+    }
+  }
+
+  return c;
 }
 
 // Do not edit this line;
--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,12 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+    if (num <= 1) {
+      return num;
+    }
+
+    return this.countDownSum(num - 1) + num;
+
   }
 }
 
